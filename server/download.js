1c7e9dff1b665bbb7dee04cbd6c166f9133ddc52
Gustavo <gustavobarboza2003@gmail.com>
2023-09-17 16:12:14 -0300

    calling the utils and creating the async functions

--- server/download.js
+++ server/download.js
@@ -1,19 +1,24 @@
 import ytdl from "ytdl-core";
 import fs from "fs";
-import { error } from "console";
 
-export const download = (videoId) => {
-  const videoURL = "https://www.youtube.com/shorts/" + videoId;
+export const download = (videoId) =>
+  new Promise((resolve, reject) => {
+    const videoURL = "https://www.youtube.com/shorts/" + videoId;
+    console.log("Realizando o download do vídeo:", videoId);
 
-  ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
-    .on("info", (info) => {
-      const seconds = info.formats[0].approxDurationMs / 1000;
+    ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
+      .on("info", (info) => {
+        const seconds = info.formats[0].approxDurationMs / 1000;
 
-      if (seconds > 60) {
-        throw new Error("A duração desse video é maior que 60 segundos.");
-      }
-    })
-    .on("end", () => {})
-    .on("error", (error) => {})
-    .pipe(fs.createWriteStream("./tmp/audio.mp4"));
-};
+        if (seconds > 60) {
+          throw new Error("A duração desse video é maior que 60 segundos.");
+        }
+      })
+      .on("end", () => {
+        resolve();
+      })
+      .on("error", (error) => {
+        reject(error);
+      })
+      .pipe(fs.createWriteStream("./tmp/audio.mp4"));
+  });
