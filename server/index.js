75b60cf35dcccaaf2e424303e07cd861c8cf3aee
Gustavo <gustavobarboza2003@gmail.com>
2023-09-18 19:57:40 -0300

    Last adjustments, AI installation, transcription and conversion

--- server/index.js
+++ server/index.js
@@ -1,24 +1,36 @@
-import cors from "cors";
-import express, { response } from "express";
+import cors from "cors"
+import express from "express"
 
-import { download } from "./download.js";
-import { transcribe } from "./transcribe.js";
-import { summarize } from "./summarize.js";
+import { convert } from "./convert.js"
+import { download } from "./download.js"
+import { transcribe } from "./transcribe.js"
+import { summarize } from "./summarize.js"
 
-const app = express();
+const app = express()
 app.use(express.json())
-app.use(cors());
+app.use(cors())
 
 app.get("/summary/:id", async (request, response) => {
-  await download(request.params.id);
-  const result = await transcribe();
+  try {
+    await download(request.params.id)
+    const audioConverted = await convert()
+    const result = await transcribe(audioConverted)
 
-  response.json({ result });
-});
+    return response.json({ result })
+  } catch (error) {
+    console.log(error)
+    return response.json({ error })
+  }
+})
 
 app.post("/summary", async (request, response) => {
-  const result = await summarize(request.body.text);
-  return response.json({ result });
-});
+  try {
+    const result = await summarize(request.body.text)
+    return response.json({ result })
+  } catch (error) {
+    console.log(error)
+    return response.json({ error })
+  }
+})
 
-app.listen(3333, () => console.log("Server is running on port 3333"));
+app.listen(3333, () => console.log("Server is running on port 3333"))
--- web/form.js
+++ web/form.js
@@ -6,7 +6,7 @@ const content = document.querySelector("#content");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
-  content.classList.add("placeholder")
+  content.classList.add("placeholder");
 
   const videoURL = input.value;
 
@@ -26,7 +26,7 @@ form.addEventListener("submit", async (event) => {
   const summary = await server.post("/summary", {
     text: transcription.data.result,
   });
-  
+
   content.textContent = summary.data.result;
-  content.classList.remove("placeholder")
+  content.classList.remove("placeholder");
 });
